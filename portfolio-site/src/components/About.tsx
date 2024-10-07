db012adfc2cde500fa67f406cb286d698324ca0d
Sam Cotroneo <sammy.cotraz@gmail.com>
2024-10-07 14:47:19 +1100

    Simplified some animations

--- portfolio-site/src/components/About.tsx
+++ portfolio-site/src/components/About.tsx
@@ -4,12 +4,17 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
         <span className="bg-gradient-to-r from-pink-300 via bg-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
           About
         </span>{" "}
         Me
-      </h2>
+      </motion.h2>
       <div className="flex flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
--- portfolio-site/src/components/Hero.tsx
+++ portfolio-site/src/components/Hero.tsx
@@ -1,6 +1,4 @@
 import { motion } from "framer-motion";
-import profilepic from "../assets/ProfilePicture.png";
-import { INTRO_CONTENT } from "../data/Content";
 
 const heroVariants = (delay: number) => ({
   hidden: { x: -100, opacity: 0 },
@@ -12,43 +10,29 @@ const Hero = () => {
     <div className="border-b border-neutral-900 pb-4 lgmb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
-            <motion.h1
-              variants={heroVariants(0)}
-              initial="hidden"
-              animate="visible"
-              className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
-            >
+          <motion.div
+            variants={heroVariants(0)}
+            initial="hidden"
+            animate="visible"
+            className="flex flex-col items-center lg:items-start"
+          >
+            <h1 className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Sam Cotroneo
-            </motion.h1>
-            <motion.span
-              variants={heroVariants(0.5)}
-              initial="hidden"
-              animate="visible"
-              className="bg-gradient-to-r from-pink-300 via bg-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
-            >
+            </h1>
+            <span className="text-2xl">Hi there! I'm a </span>
+            <span className="pb-8 bg-gradient-to-r from-pink-300 via bg-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Software Engineer
-            </motion.span>
-            <motion.p
-              variants={heroVariants(1)}
-              initial="hidden"
-              animate="visible"
-              className="my-2 max-w-xl py-6 font-light tracking-tighter "
-            >
-              {INTRO_CONTENT}
-            </motion.p>
-          </div>
+            </span>
+          </motion.div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-          <div className="flex justify-center">
-            <motion.img
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 1.2 }}
-              src={profilepic}
-              alt="Profile Picture"
-            />
-          </div>
+        <div className="w-full lg:w-1/2 flex items-center justify-center">
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0 }}
+          >
+            <Profile />
+          </motion.div>
         </div>
       </div>
     </div>
